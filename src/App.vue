80d653d1b58b7b0cb75facad8d56837db58544f1
arafatislam123 <arafatpstu7@gmail.com>
2024-12-22 20:14:56 +0600

    add props and more functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script setup>
-const color='yellow'
+import {ref} from 'vue'
+const color=ref('red')
+const textSize=ref('43')
 </script>
 
 <template>
   <button class="text-white font-bold py-2 px-4 rounded" :class="[
     `bg-${color}-500`,
-    `hover:bg-${color}-700`,
-  ]">
+    `hover:bg-${color}-700`
+
+  ]"
+           :style=" `font-size: ${textSize}px`"
+  >
     Button
 
   </button>
+  <div class="mt-20">
+    <input class="border border-gray-400 p-2" type="text" v-model="color" placeholder="color"/>
+    <input class="border border-gray-400 p-2" type="text" v-model="textSize" placeholder="textSize"/>
+
+
+  </div>
 
 </template>
 
